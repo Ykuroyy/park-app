1e9e2589f2e5b90d201107f0c16afa63a410c39d
Ykuroyy <yykm666@gmail.com>
2025-09-01 19:45:35 +0900

    Switch to direct Webpack build for Netlify deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,17 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+const path = require('path');
 
 module.exports = async function (env, argv) {
-  const config = await createExpoWebpackConfigAsync(env, argv);
+  const config = await createExpoWebpackConfigAsync(
+    {
+      ...env,
+      projectRoot: __dirname,
+    },
+    argv
+  );
+  
+  // 出力ディレクトリを設定
+  config.output.path = path.resolve(__dirname, 'web-build');
+  
   return config;
 };
